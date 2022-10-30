6f63b8417a245f3a2ba48222fcacff31a1cddd2f
shegspear <shegspear@gmail.com>
2022-10-30 11:30:02 +0000

    built reuseable parent layout compnent with responsiveness

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import { Link } from 'react-router-dom';
 import { 
     AiOutlineAppstore, 
-    AiOutlineCreditCard 
+    AiOutlineCreditCard,
+    AiOutlineClose 
 } from 'react-icons/ai';
 import { TbCapture } from 'react-icons/tb';
-import { RiHandCoinLine } from 'react-icons/ri';
+import { RiHandCoinLine, RiNotification2Fill } from 'react-icons/ri';
 import {FiSettings} from 'react-icons/fi';
+import {BsChevronDown, BsChevronUp} from 'react-icons/bs';
+import {BiMenuAltRight} from 'react-icons/bi';
 import { Image } from 'react-bootstrap';
+import Overlay from 'react-bootstrap/Overlay';
+import Popover from 'react-bootstrap/Popover';
 
 import './Styles.css';
 
@@ -15,6 +20,23 @@ function Layout({
     children,
     currentPage
 }) {
+    const [mobileNav, setMobileNav] = useState(false);
+    const [show, setShow] = useState(false);
+    const [target, setTarget] = useState(null);
+    const ref = useRef(null);
+  
+    const handleClick = (event) => {
+      setShow(!show);
+      setTarget(event.target);
+    };
+
+    const navList =[
+        'Dashboard',
+        'Transactions',
+        'Accounts',
+        'Payment',
+        'Settings'
+    ]
   return (
     <div className='layout-cont'>
         <div className='side-nav'>
@@ -27,77 +49,148 @@ function Layout({
             </div>
 
             <div className='side-nav-link-cont'>
-                <Link 
-                    className='side-nav-link'
-                    style={{
-                        color: currentPage === 'dashboard' ? 'rgba(3, 166, 60, 1)' : '#828282;',
-                        background: currentPage === 'dashboard' ? 'rgba(235, 241, 237, 1)' : 'transparent'
-                    }}
-                    to={'/dashboard'}
-                >
-                    <AiOutlineAppstore style={{marginRight: 10, fontSize: 20}} />
-                    Dashboard
-                </Link>
+                {
+                    navList.map((link, index) => (
+                        <Link
+                            key={index} 
+                            className='side-nav-link'
+                            style={{
+                                color: currentPage.toLowerCase() === link.toLowerCase() ? 'rgba(3, 166, 60, 1)' : '#828282',
+                                background: currentPage.toLowerCase() === link.toLowerCase() ? 'rgba(235, 241, 237, 1)' : 'transparent'
+                            }}
+                            to={`/${link.toLowerCase()}`}
+                        >
+                            {
+                                link.toLowerCase() === 'dashboard' ? (
+                                    <AiOutlineAppstore style={{marginRight: 10, fontSize: 20}} />
+                                ) :
+                                link.toLowerCase() === 'transactions' ? (
+                                    <AiOutlineCreditCard style={{marginRight: 10, fontSize: 20}} /> 
+                                ) :
+                                link.toLowerCase() === 'accounts' ? (
+                                    <TbCapture style={{marginRight: 10, fontSize: 20}} /> 
+                                ) :
+                                link.toLowerCase() === 'payment' ? (
+                                    <RiHandCoinLine style={{marginRight: 10, fontSize: 20}} />
+                                ) : 
+                                link.toLowerCase() === 'settings' ? (
+                                    <FiSettings style={{marginRight: 10, fontSize: 20}} />
+                                ) : (<div></div>)
 
-                <Link 
-                    className='side-nav-link'
-                    style={{
-                        color: currentPage === 'transactions' ? 'rgba(3, 166, 60, 1)' : '#828282;',
-                        background: currentPage === 'transactions' ? 'rgba(235, 241, 237, 1)' : 'transparent'
-                    }}
-                    to={'/transactions'}
-                >
-                    <AiOutlineCreditCard style={{marginRight: 10, fontSize: 20}} />
-                    Transactions
-                </Link>
-
-                <Link 
-                    className='side-nav-link'
-                    style={{
-                        color: currentPage === 'accounts' ? 'rgba(3, 166, 60, 1)' : '#828282;',
-                        background: currentPage === 'accounts' ? 'rgba(235, 241, 237, 1)' : 'transparent'
-                    }}
-                    to={'/accounts'}
-                >
-                    <TbCapture style={{marginRight: 10, fontSize: 20}} />
-                    Accounts
-                </Link>
-                
-                <Link 
-                    className='side-nav-link'
-                    style={{
-                        color: currentPage === 'payment' ? 'rgba(3, 166, 60, 1)' : '#828282;',
-                        background: currentPage === 'payment' ? 'rgba(235, 241, 237, 1)' : 'transparent'
-                    }}
-                    to={'/payment'}
-                >
-                    <RiHandCoinLine style={{marginRight: 10, fontSize: 20}} />
-                    Payment
-                </Link>
-
-                <Link 
-                    className='side-nav-link'
-                    style={{
-                        color: currentPage === 'settings' ? 'rgba(3, 166, 60, 1)' : '#828282;',
-                        background: currentPage === 'settings' ? 'rgba(235, 241, 237, 1)' : 'transparent'
-                    }}
-                    to={'/settings'}
-                >
-                    <FiSettings style={{marginRight: 10, fontSize: 20}} />
-                    Settings
-                </Link>
+                            }
+                            {link}
+                        </Link> 
+                    ))
+                }
             </div>
         </div>
         <div className='main-body'>
             <div className='head-nav'>
-               <div className='menu-box'>
-                
+                <div className='head-nav-logo-cont'>
+                    <Image 
+                        src='assets/Logo.svg' 
+                        alt="logo"
+                        className='head-nav-logo' 
+                    />
+                </div>
+
+               <div ref={ref} className='menu-box'>
+
+                    <div className='bell-cont'>
+                        <div className='bell-badge'></div>
+                        <RiNotification2Fill style={{fontSize: 20, color: '#C5C7CD'}} />
+                    </div>
+
+                    <div 
+                        className='nav-profile'
+                        onClick={handleClick}
+                    >
+                        <Image 
+                            src='assets/place-holder/profile-circle.svg' 
+                            alt='profile picture'
+                            style={{
+                                width: 44,
+                                height: 44,
+                                borderRadius: '50%'
+                            }}
+                        />
+
+                        <p className='profile-name'>
+                            Ikebe inc
+                        </p>
+
+                        {
+                            !show ? (
+                                <BsChevronDown />
+                            ) : (
+                                <BsChevronUp />
+                            )
+                        }
+
+                    </div>
+
+                    <Overlay
+                        show={show}
+                        target={target}
+                        placement="bottom"
+                        container={ref}
+                        containerPadding={20}
+                    >
+                        <Popover id="popover-contained">
+                        <Popover.Header as="h3">Profile</Popover.Header>
+                        <Popover.Body>
+                            <strong>Do something</strong> not entirely sure.
+                        </Popover.Body>
+                        </Popover>
+                    </Overlay>
+
+
+                    <div 
+                        className='mobile-nav-btn-cont'
+                        onClick={() => setMobileNav(true)}
+                    >
+                        <BiMenuAltRight  
+                            style={{
+                                color: 'rgba(3, 166, 60, 1)',
+                                fontSize: 30,
+                            }}
+                        />
+                    </div>
                </div> 
             </div>
             <div className='children-body'>
                 {children}
             </div>
         </div>
+        {
+            mobileNav && (
+                <div className='mobile-nav'>
+                    <div className='mobile-nav-list'>
+                        {
+                            navList.map((link, index) => (
+                                <Link
+                                    key={index} 
+                                    className='mobile-nav-link'
+                                    style={{
+                                        color: currentPage.toLowerCase() === link.toLowerCase() ? 'rgba(3, 166, 60, 1)' : '#fff;',
+                                    }}
+                                    to={`/${link.toLowerCase()}`}
+                                >
+                                    {link}
+                                </Link> 
+                            ))
+                        }
+                    </div>
+
+                    <div 
+                        className='mobile-nav-close-cont'
+                        onClick={() => setMobileNav(false)}
+                    >
+                        <AiOutlineClose style={{fontSize: 30, color: '#fff'}} />
+                    </div>
+                </div>
+            )
+        }
     </div>
   );
 };
