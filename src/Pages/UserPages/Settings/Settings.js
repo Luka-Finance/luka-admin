4115c787ede977266c8a9a5a22df90215942e0a9
shegspear <shegspear@gmail.com>
2022-11-14 20:42:17 +0100

    integrated rc number and tin verification endpoints

--- src/Pages/UserPages/Settings/Settings.js
+++ src/Pages/UserPages/Settings/Settings.js
@@ -1,16 +1,114 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AuxPageHead from '../../../Components/AuxPageHead/AuxPageHead';
 import Layout from '../../../Components/Layout/Layout';
 import Input from '../../../Components/Common/Input/Input';
 import CustomSelector from '../../../Components/Common/CustomSelector/CustomSelector';
 import {BsCloudArrowUp, BsExclamationCircle} from 'react-icons/bs';
 import CustomButton from '../../../Components/Common/CustomButton/Index';
+import { toast, ToastContainer } from 'react-toastify';
+import LoaderScreen from '../../../Components/Common/LoaderScreen/LoaderScreen';
+import 'react-toastify/dist/ReactToastify.css';
+import axiosInstance from '../../../Utils/axiosInstance';
 
 import './Styles.css';
 
 function Settings() {
+  const [form, setForm] = useState({});
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [loadingRC, setLoadingRC] = useState(false);
+  const [loadingTIM, setLoadingTIN] = useState(false);
+
+  const onEnterValue = ({name, value}) => { 
+    setForm({...form, [name]: value});
+
+    if(value !== '') {
+
+        if(name === 'companyName') {
+
+            if(value.length < 3) {
+                setErrors(prev => {return {...prev, [name]: `Company name should be a minimum of 3 characters`}});
+              } else {
+                setErrors(prev => {return {...prev, [name]: null}});
+            };
+
+        } else if (name === 'rcNumber') {
+
+            if(value.length < 7) {
+                setErrors(prev => {return {...prev, [name]: `R.C number should be 7 characters long.`}});
+              } else {
+                setErrors(prev => {return {...prev, [name]: null}});
+            };
+
+        } else if (name === 'tinNumber') {
+
+            if(value.length < 7) {
+                setErrors(prev => {return {...prev, [name]: `TIN number should be 7 characters long.`}});
+              } else {
+                setErrors(prev => {return {...prev, [name]: null}});
+            };
+
+        }
+
+    } else {
+        setErrors(prev => {return {...prev, [name]: `This field is required`}});  
+    }
+  };
+
+  const verifyRcNUmber = async(value) => {
+    if(value.length === 7) {
+        setLoadingRC(true);
+        try {
+            const res = await axiosInstance({
+                url: '/business/verify/rc',
+                method: 'POST',
+                body: {
+                    companyName: form.companyName,
+                    rcNumber: form.rcNumber
+                }
+            });
+            console.log('res ', res);
+            setLoadingRC(false);
+        } catch (error) {
+            setLoadingRC(false);
+            setErrors(prev => {return {...prev, ['rcNumber']: `${error.message}`}});
+            toast.error(error.message, {
+                position: toast.POSITION.TOP_RIGHT
+            })
+            return(<ToastContainer />)
+        }
+    }
+  };
+
+  const verifyTin = async(value) => {
+    if(value.length === 7) {
+        setLoadingTIN(true);
+        try {
+            const res = await axiosInstance({
+                url: '/business/verify/tin',
+                method: 'POST',
+                body: {
+                    searchParameter: form.tinNumber,
+                }
+            });
+            console.log('res ', res);
+            setLoadingTIN(false);
+        } catch (error) {
+            setLoadingTIN(false);
+            setErrors(prev => {return {...prev, ['tinNumber']: `${error.message}`}});
+            toast.error(error.message, {
+                position: toast.POSITION.TOP_RIGHT
+            })
+            return(<ToastContainer />)
+        }
+    }
+  };
+
   return (
     <Layout currentPage={'settings'}>
+        {/* for toast notification containing */}
+        <ToastContainer />
+
         <AuxPageHead 
          auxHeadFilter={false}
          auxHeadTitle={'Settings'}
@@ -30,9 +128,40 @@ function Settings() {
                         <Input 
                             label={'Company name'}
                             type={'text'}
-                            onChange={(e) => console.log(e)}
-                            error={''}
+                            onChange={(e) => {
+                                const value = e.target.value;
+                                onEnterValue({name: 'companyName', value})
+                            }}
+                            error={errors.companyName}
                         />
+                    </div>
+
+                    <div className='settings-input-cont'>
+                        <Input 
+                            label={'RC Number'}
+                            type={'text'}
+                            onChange={(e) => {
+                                const value = e.target.value;
+                                onEnterValue({name: 'rcNumber', value});
+                                verifyRcNUmber(value);
+                            }}
+                            error={errors.rcNumber}
+                        />
+                        {loadingRC && (<h5 style={{color: 'green'}}>Checking R.C number....</h5>)}
+                    </div>
+
+                    <div className='settings-input-cont'>
+                        <Input 
+                            label={'TIN Number'}
+                            type={'text'}
+                            onChange={(e) => {
+                                const value = e.target.value;
+                                onEnterValue({name: 'tinNumber', value});
+                                verifyTin(value);
+                            }}
+                            error={errors.tinNumber}
+                        />
+                        {loadingTIM && (<h5 style={{color: 'green'}}>Checking TIN number....</h5>)}
                     </div>
 
                     <div className='settings-input-cont'>
