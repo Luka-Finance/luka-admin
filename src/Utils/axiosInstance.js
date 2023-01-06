9f45467a7c4a7bf49bf7a9c385f25cf6ac4f5ae3
Emmanuel Adelugba <adelugba.emma@gmail.com>
2023-01-06 21:44:40 +0100

    ...

--- src/Utils/axiosInstance.js
+++ src/Utils/axiosInstance.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
-import accessLocalStorage from './accessLocalStorage';
+import axios from 'axios'
+import accessLocalStorage from './accessLocalStorage'
 
 const getAccessToken = () => {
- const token = accessLocalStorage.getFromLs('token');
- if(token) {
-  return `Beare ${token}`;
- } else {
-  return ``;
- }
-};
+	const token = accessLocalStorage.getFromLs('token')
+	if (token) {
+		return `Beare ${token}`
+	} else {
+		return ``
+	}
+}
 
 // const axiosInstance = axios.create({
 //   baseURL : 'https://luka-api.vercel.app',
@@ -16,40 +16,46 @@ const getAccessToken = () => {
 //     Authorization: getAccessToken(),
 //     "Content-Type": "application/json",
 //     timeout : 1000,
-//   }, 
+//   },
 // });
 
 const axiosInstance = axios.create({
-  baseURL : 'https://luka-api.vercel.app',
-});
+	baseURL: 'https://api.luka.finance',
+})
 
-axiosInstance.interceptors.request.use(async (config) => {
-  // Do something before request is sent
-  config.headers = {
-    Authorization: getAccessToken(),
-    "Content-Type": "application/json",
-    timeout : 1000, 
-  }
-  return config;
-}, (error) => {
-  // Do something with request error
-  return Promise.reject(error);
-});
+axiosInstance.interceptors.request.use(
+	async (config) => {
+		// Do something before request is sent
+		config.headers = {
+			Authorization: getAccessToken(),
+			'Content-Type': 'application/json',
+			timeout: 1000,
+		}
+		return config
+	},
+	(error) => {
+		// Do something with request error
+		return Promise.reject(error)
+	}
+)
 
-axiosInstance.interceptors.response.use(function (response) {
-  // Any status code that lie within the range of 2xx cause this function to trigger
-  // Do something with response data
-  return response;
-}, function (error) {
-  // Any status codes that falls outside the range of 2xx cause this function to trigger
-  // Do something with response error
-  if(error.response.data.message === 'Token Expired') {
-    accessLocalStorage.clearLs();
-    window.location.replace('/sign-in');
-  } else {
-    console.log('we good')
-  }
-  return Promise.reject(error);
-});
+axiosInstance.interceptors.response.use(
+	function (response) {
+		// Any status code that lie within the range of 2xx cause this function to trigger
+		// Do something with response data
+		return response
+	},
+	function (error) {
+		// Any status codes that falls outside the range of 2xx cause this function to trigger
+		// Do something with response error
+		if (error.response.data.message === 'Token Expired') {
+			accessLocalStorage.clearLs()
+			window.location.replace('/sign-in')
+		} else {
+			console.log('we good')
+		}
+		return Promise.reject(error)
+	}
+)
 
-export default axiosInstance;
+export default axiosInstance
